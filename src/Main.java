9785e5c050403639c43569219ec127dba8ea5e15
Birkan32 <85389327+Birkan32@users.noreply.github.com>
2021-07-18 14:12:08 +0300

    elmas sekli olusturuldu

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner klavye = new Scanner(System.in);
+        System.out.print("yükseklik kaç satır olsun?   :");
+        int yukseklik = klavye.nextInt(); //yükseklik hep tek sayi olacaktir
+        int yariyukseklik = (int) (yukseklik / 2) + 1;
+
+        for (int i = 1; i <= yariyukseklik; i++) {
+            for (int j = 1; j <= yariyukseklik - i; j++)
+                System.out.print(" ");
+
+            for (int j = 1; j <= 2 * i - 1; j++)
+                System.out.print("*");
+
+            System.out.println();
+        }
+        //üst yarısı tamam
+        for (int i = yariyukseklik - 1; i >= 1; i--) {
+            for (int j = 1; j <= yariyukseklik - i; j++)
+                System.out.print(" ");
+
+            for (int j = 1; j <= 2 * i - 1; j++)
+                System.out.print("*");
+
+            System.out.println();
+        }
+
+    }
+}
